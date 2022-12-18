17ba365ef73b8dbd2c93c6c4f3566737b62b539d
Bnaya <bnaya98@gmail.com>
2022-12-18 22:04:42 +0200

    user add

--- api/user/user.routes.js
+++ api/user/user.routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 // const {requireAuth} = require('../../middlewares/requireAuth.middleware')
-const {getUser, getUsers, deleteUser, updateUser} = require('./user.controller')
+const { getUser, getUsers, deleteUser, updateUser, addUser } = require('./user.controller')
 const router = express.Router()
 
 // middleware that is specific to this router
@@ -9,7 +9,7 @@ const router = express.Router()
 router.get('/', getUsers)
 router.get('/:id', getUser)
 router.put('/:id', updateUser)
-
+router.post('/', addUser)
 router.delete('/:id', deleteUser)
 
 module.exports = router
